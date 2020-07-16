b160fc02ca9bfe6ab0c930d4a0bfa491f185a8cc
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-16 22:19:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
   else
-    number=(2..num-1).to_a
-    number.all? do |possible_factor|
-      num % possible_factor != 0
+    number=(2..num-1).to_a #change range to an array (to_a)
+    number.all? do |n|
+      num % n != 0
     end
   end
 end
